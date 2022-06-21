9a27c80a9dafd0f768f92f62ae2f4bce8f13fa59
destx0 <destisdbest@gmail.com>
2022-06-21 22:24:09 +0530

    prob 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(given) {
+    let out = ""
+    for (let i = given.length -1; i >= 0; i--) {
+        out += given[i]
+    }
+     return out   
 };
 
 // Do not edit below this line
