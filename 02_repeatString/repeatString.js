0d3b546528206c29ab96291362f68e17c0b4ad23
destx0 <destisdbest@gmail.com>
2022-06-21 22:12:57 +0530

    prob 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function (word, num) {
+  tmp = "";
+  if (num<0) {
+    return "ERROR"
+  }
+  for (let i = 0; i < num; i++) {
+    tmp += word;
+  }
+  return tmp;
 };
 
 // Do not edit below this line
