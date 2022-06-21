a977d95d380a823ee91062e117ea33ef757ac841
destx0 <destisdbest@gmail.com>
2022-06-21 22:40:26 +0530

    prob 4 error

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr,rem) {
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] == rem) {
+            arr.splice(i, 1);
+            return arr
+        }
+        
+    }
 };
 
 // Do not edit below this line
